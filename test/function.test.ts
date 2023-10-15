996b664282ef2ddbc9084a6223f83a67e1457c3c
mudssky <mudssky@gmail.com>
2023-10-15 16:52:21 +0800

    feat: 🎸 debouce v1

--- test/function.test.ts
+++ test/function.test.ts
@@ -27,26 +27,42 @@ describe('debounce', () => {
     expect(fn).toBeCalled()
     expect(fn).toHaveBeenCalledWith(1, 2, 3)
   })
-  // tariling和leading都为false时，应该可以在延迟·时间过后，手动调用函数
-  test.skip('can manually execute,when tariling and leading is false', () => {
+  test('should reset timer when call function  during wait', () => {
     const fn = vi.fn()
-    const debouncedFn = debounce(fn, 1000, {
-      trailing: false,
-      leading: false,
-    })
-    // 时间未到前不能调用
+    const debouncedFn = debounce(fn, 1000)
     debouncedFn(1, 2, 3)
     expect(fn).not.toBeCalled()
-    vi.advanceTimersByTime(100)
+    vi.advanceTimersByTime(200)
     expect(fn).not.toBeCalled()
-    vi.advanceTimersByTime(100)
+    vi.advanceTimersByTime(700)
     expect(fn).not.toBeCalled()
-    // 时间到了，可以手动调用
-    vi.advanceTimersByTime(800)
     debouncedFn(1, 2, 3)
-    expect(fn).toBeCalled()
-    // expect(fn).toHaveBeenCalledWith(1, 2, 3)
+    vi.advanceTimersByTime(1000)
+    expect(fn).toHaveBeenCalledWith(1, 2, 3)
   })
+  // tariling和leading都为false时，应该可以在延迟·时间过后，手动调用函数
+  test.concurrent(
+    'can manually execute,when tariling and leading is false',
+    () => {
+      const fn = vi.fn()
+      const debouncedFn = debounce(fn, 1000, {
+        trailing: false,
+        leading: false,
+      })
+      // 时间未到前不能调用
+      debouncedFn(1, 2, 3)
+      expect(fn).not.toBeCalled()
+      vi.advanceTimersByTime(100)
+      expect(fn).not.toBeCalled()
+      vi.advanceTimersByTime(100)
+      expect(fn).not.toBeCalled()
+      // 时间到了，可以手动调用
+      vi.advanceTimersByTime(800)
+      debouncedFn(1, 2, 3)
+      expect(fn).toBeCalled()
+      // expect(fn).toHaveBeenCalledWith(1, 2, 3)
+    },
+  )
 
   test.concurrent(
     'should only excute function first,when leading is true,trailing is false',
@@ -67,7 +83,7 @@ describe('debounce', () => {
       debouncedFn(1, 2, 3)
       expect(fn).toHaveBeenCalledTimes(1)
       //延时结束，不会调用trailing
-      vi.advanceTimersByTime(800)
+      vi.advanceTimersByTime(1000)
       expect(fn).toHaveBeenCalledTimes(1)
       // 手动触发第一次leading调用
       debouncedFn(1, 2, 3)
@@ -108,16 +124,16 @@ describe('debounce', () => {
       const fn = vi.fn()
       const debouncedFn = debounce(fn, 1000, {
         trailing: true,
-        leading: false,
+        leading: true,
       })
       const args = [1, 2, 3]
       // 会立刻执行
       debouncedFn(...args)
       expect(fn).toHaveBeenNthCalledWith(1, ...args)
       vi.advanceTimersByTime(100)
-      expect(fn).not.toBeCalled()
+      expect(fn).toHaveBeenNthCalledWith(1, ...args)
       vi.advanceTimersByTime(100)
-      expect(fn).not.toBeCalled()
+      expect(fn).toHaveBeenNthCalledWith(1, ...args)
 
       // 时间到了会自动执行
       vi.advanceTimersByTime(800)
@@ -129,4 +145,51 @@ describe('debounce', () => {
       expect(fn).toHaveBeenNthCalledWith(3, ...args)
     },
   )
+
+  test.concurrent('should support cancel', () => {
+    const fn = vi.fn()
+    const debouncedFn = debounce(fn, 1000)
+    const args = [1, 2, 3]
+    // 不会立刻执行
+    debouncedFn(...args)
+    expect(fn).not.toBeCalled()
+    vi.advanceTimersByTime(100)
+    expect(fn).not.toBeCalled()
+    vi.advanceTimersByTime(100)
+    expect(fn).not.toBeCalled()
+
+    expect(debouncedFn.pending()).toBe(true)
+    // 提前取消
+    debouncedFn.cancel()
+    expect(debouncedFn.pending()).toBe(false)
+    vi.advanceTimersByTime(800)
+
+    expect(fn).not.toBeCalled()
+    // 手动触发
+    debouncedFn(...args)
+    expect(debouncedFn.pending()).toBe(true)
+    vi.advanceTimersByTime(1000)
+    expect(debouncedFn.pending()).toBe(false)
+    expect(fn).toHaveBeenNthCalledWith(1, ...args)
+  })
+
+  test.concurrent('should support flush', () => {
+    function sum(...args: number[]) {
+      return args.reduce((a, b) => a + b, 0)
+    }
+    const args = [1, 2, 3]
+
+    const debouncedFn = debounce(() => sum(...args), 1000)
+    debouncedFn()
+    vi.advanceTimersByTime(100)
+    expect(debouncedFn.pending()).toBe(true)
+    const res = debouncedFn.flush()
+    expect(debouncedFn.pending()).toBe(false)
+    expect(res).toBe(6)
+    // 测试trailing执行后，再执行flush
+    debouncedFn()
+    vi.advanceTimersByTime(1000)
+    const res2 = debouncedFn.flush()
+    expect(res2).toBe(6)
+  })
 })
