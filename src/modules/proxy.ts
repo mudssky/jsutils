53404081d7d3c2368208523d8e34648e12f80ac3
mudssky <mudssky@gmail.com>
2023-10-26 15:45:06 +0800

    feat: 🎸 add singletonProxy

---
+++ src/modules/proxy.ts
@@ -0,0 +1,24 @@
+/**
+ * 传入一个对象，利用Proxy包装返回一个单例对象
+ * 因为使用了Proxy，所以this绑定还是原来的对象不会受影响
+ * @param obj
+ * @returns
+ * @public
+ * @example
+ * ```ts
+ *
+ * ```
+ */
+export function singletonProxy<T extends object>(
+  obj: new (...args: unknown[]) => T,
+) {
+  let instance: T
+  return new Proxy(obj, {
+    construct(target, argArray) {
+      if (!instance) {
+        instance = new target(...argArray)
+      }
+      return instance
+    },
+  })
+}
